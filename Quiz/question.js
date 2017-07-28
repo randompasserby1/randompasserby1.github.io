569659d4c0559af97a8219df0ab1c7b03ca3d794
randompasserby1 <giangle@techmasters-Mac-mini-6.local>
2017-07-28 11:09:16 +0700

    BT BOM

---
+++ Quiz/question.js
@@ -0,0 +1,36 @@
+var questions = [{
+	"question": "Which US President was in office during the Cuban Missile Crisis?",
+	"option1": "George Washington",
+	"option2": "Abraham Lincoln",
+	"option3": "John F.Kennedy",
+	"option4": "Barack Obama",
+	"answer": "3"
+}, {
+	"question": "What number of points come after 15 in a game of tennis",
+	"option1": "16",
+	"option2": "25",
+	"option3": "30",
+	"option4": "40",
+	"answer": "3"
+}, {
+	"question": "On average, which of these bones is longer?",
+	"option1": "Humerus",
+	"option2": "Tibia",
+	"option3": "Ulna",
+	"option4": "Sternum",
+	"answer": "2"
+}, {
+	"question": "What are the Secret Service names for Barack and Michelle Obama?",
+	"option1": "Renegade and Renaissance",
+	"option2": "BO and MO",
+	"option3": "Bar-rock and Mich",
+	"option4": "All of the above",
+	"answer": "1"
+}, {
+	"question": "Diospyros Kaki is the Latin name for what fruit?",
+	"option1": "Persimmon",
+	"option2": "Plum",
+	"option3": "Pineapple",
+	"option4": "Papaya",
+	"answer": "3"
+}];
